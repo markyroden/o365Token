b2819ac3524b689a82488565ef59a0a55f73ece4
mroden <marky.roden@gmail.com>
2016-04-19 20:26:37 -0500

    Added O365Token_Extension example

---
+++ O365Token_Extension/scripts/App.js
@@ -0,0 +1,95 @@
+﻿// Common app functionality
+
+var app = (function () {
+    'use strict';
+
+    var app = {};
+
+    //Change this value for every Add-In
+    app.addinName = "OAuthToken"
+
+    // Function called when the page has opened in the Outlook Add-In
+    app.getToken = function (tokenParams) {
+
+        var url =   tokenParams.authServer +
+            "response_type=" + encodeURI(tokenParams.responseType) + "&" +
+            "client_id=" + encodeURI(tokenParams.clientId) + "&" +
+            "resource=" + encodeURI(tokenParams.resource) + "&" +
+            "redirect_uri=" + encodeURI(tokenParams.replyUrl);
+
+        var winObj = window.open(url);
+        winObj.focus();
+
+    };
+
+    //Function called when the page has been called post token-generation in the pop-up window
+    app.returnToken = function(){
+        var urlParameterExtraction = new (function () {
+            function splitQueryString(queryStringFormattedString) {
+                var split = queryStringFormattedString.split('&');
+
+                // If there are no parameters in URL, do nothing.
+                if (split == "") {
+                    return {};
+                }
+
+                var results = {};
+
+                // If there are parameters in URL, extract key/value pairs.
+                for (var i = 0; i < split.length; ++i) {
+                    var p = split[i].split('=', 2);
+                    if (p.length == 1)
+                        results[p[0]] = "";
+                    else
+                        results[p[0]] = decodeURIComponent(p[1].replace(/\+/g, " "));
+                }
+                return results;
+            }
+
+            // Split the query string (after removing preceding '#').
+            this.queryStringParameters = splitQueryString(window.location.hash.substr(1));
+        })();
+
+        var token = urlParameterExtraction.queryStringParameters['access_token'];
+
+        if (window.opener){
+            app.setCookie(token, 3600);
+            window.opener.console.log('cookie set')
+            window.opener.app.tokenCallback(token);
+            window.close()
+        } else {
+            document.write("There appears to have been an error, please close the window and check with your administrator")
+        }
+
+    };
+
+    //The code which runs the Add-In itself
+    app.tokenCallback = function(token){
+        //this would then continue to do what you really need in the Add-In
+        console.log('this is what happens next.....')
+    };
+
+    app.setCookie = function(value, duration) {
+        var now = new Date();
+        var time = now.getTime();
+        var expireTime = time + 1000*duration;
+        now.setTime(expireTime);
+        console.log("---setting cookie--");
+        document.cookie = app.addinName+"="+value+';expires='+now.toGMTString()+';path=/';
+        console.log(document.cookie);
+    }
+
+    app.getCookie = function (addinName, name){
+        var pattern = RegExp(name + "=.[^;]*")
+        var matched = document.cookie.match(pattern)
+        if(matched){
+            var cookie = matched[0].split('=')
+            return cookie[1]
+        }
+        return false
+    }
+
+
+    return app;
+})();
+
---
+++ O365Token_Extension/scripts/Home.js
@@ -0,0 +1,37 @@
+﻿
+(function () {
+    'use strict';
+    var token = app.getCookie("OAuthToken")
+    //check to see if there is an OAuth Token cached in the cookie
+    //we check the URL because the first time the page is returned from Authorization
+    //the cookie has not been set - without this check we would open extra windows......
+    if (!token && (location.href.split('access_token').length<2)){
+        $(document).ready(function () {
+            if (!token) {
+                var tokenParams = {};
+                tokenParams.authServer = 'https://login.windows.net/common/oauth2/authorize?';
+                tokenParams.responseType = 'token';
+                //where the OAuth process sends the web page back to once the token
+                //has been successfully generated
+                tokenParams.replyUrl = "https://xomino365.com";
+
+                //THESE tokenParams need to be changed for your application
+                tokenParams.clientId = '1bc6eb91-a9b4-47ee-9add-7d14ff5d77bd';
+                //this is the O365 site from where you are going to get data
+                tokenParams.resource = "https://xomino.sharepoint.com";
+
+                app.getToken(tokenParams);
+            } else {
+                //we have a token therefore carry on
+                app.tokenCallback(token)
+            }
+        });
+    } else {
+        //The window has opened for token generation
+        $(document).ready(function () {
+            app.returnToken();
+        });
+    }
+})();
+
+
